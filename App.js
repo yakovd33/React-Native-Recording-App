20f0bf0001c710ecb788ffd23d266bc24621b324
Yakov Shitrit <yakovd33@gmail.com>
2020-09-19 09:19:08 +0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AudioRecord from 'react-native-audio-record';
+import { Audio } from 'expo-av';
+
+Audio.requestPermissionsAsync();
 
 export default function App() {
+ 
+  const options = {
+    sampleRate: 16000,  // default 44100
+    channels: 1,        // 1 or 2, default 1
+    bitsPerSample: 16,  // 8 or 16, default 16
+    audioSource: 6,     // android only 
+    wavFile: 'test.wav' // default 'audio.wav'
+  };
+  
+  AudioRecord.init(options);
+
+  //Start Recording
+  AudioRecord.start();
+  
+  
+  //Stop Recording
+  AudioRecord.stop(); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text selectable selectionColor="blue" color="#fff">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//const element = <Mic record="true" />;
+//ReactDOM.render(element, document.getElementById('root'));
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Models/Mic.js
@@ -0,0 +1,14 @@
+  
+import { NativeModules, NativeEventEmitter } from 'react-native';
+
+const { MicrophoneStream } = NativeModules;
+const emitter = new NativeEventEmitter(MicrophoneStream);
+
+export default {
+  // TODO: params check
+  init: (options) => MicrophoneStream.init(options),
+  start: () => MicrophoneStream.start(),
+  pause: () => MicrophoneStream.pause(),
+  stop: () => MicrophoneStream.stop(),
+  addListener: listener => emitter.addListener('audioData', listener),
+};
